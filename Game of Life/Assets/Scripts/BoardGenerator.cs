f1a4e8ac299511ed351d77bedfd31457bb50f4e7
acanady <alfonso.canadyjr@gmail.com>
2021-11-08 06:20:36 -0600

    added board generator script

---
+++ Game of Life/Assets/Scripts/BoardGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BoardGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+    public int size;
+    private GameObject section;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for(int j =  0; j < size; j++)
+            {
+                section = GameObject.Instantiate(prefab, new Vector3(i + i * .1f, 0, j + j * .1f), Quaternion.identity);
+                section.transform.SetParent(this.gameObject.transform);
+            }
+            
+        }
+    }
+}
